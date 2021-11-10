89fc6a0520ca0d3352a0103aaa6d2efa09a0dada
Mehul Shreenath <mehulshreenath@protonmail.com>
2021-11-10 21:08:26 +0530

    numpy

---
+++ numberspy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def main():
+    my_list = [1, 2, 3]
+    print(my_list)
+    print(np.array(my_list))
+    print(np.arange(0, 10))
+    print(np.ones(1))
+
+
+if __name__ == '__main__':
+    main()
